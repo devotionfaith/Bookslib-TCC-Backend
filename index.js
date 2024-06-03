97aa956d6db439f7d70f3c1b5e2bad45668d14dc
devotionfaith <rgusti530@gmail.com>
2024-06-04 03:57:41 +0700

    lagi

--- index.js
+++ index.js
@@ -19,4 +19,4 @@ app.use(cors({credentials: true, origin:'http://localhost:3000'}));
 app.use(cookieParser());
 app.use(express.json());
 app.use(router);
-app.listen(8080, ()=> console.log('Server running at port 5000'))
+app.listen(8080, ()=> console.log('Server running at port 8080'))
